3e94019521e67bfa8d2c2d514cb64b77b59d2be9
chaiwenxin1 <2249816238@qq.com>
2020-10-18 17:23:20 +0800

    work2

---
+++ build/precache-manifest.f99077c95493b7b9cc9dc73f74596714.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76beffee3d5684fb5c00010a50eccca6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7928d4b9f3936604e1d7",
+    "url": "/static/css/main.4fea05f6.chunk.css"
+  },
+  {
+    "revision": "7ede5f113c81ae69df7a",
+    "url": "/static/js/2.545e65b1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.545e65b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7928d4b9f3936604e1d7",
+    "url": "/static/js/main.dd1ec009.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
